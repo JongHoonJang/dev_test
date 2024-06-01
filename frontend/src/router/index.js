3fe290fa65f0ca901f47b99aaab8b3bcfeaa32a8
JongHoonJang <jangjr98@gmail.com>
2024-06-01 20:59:06 +0900

    로우터가드 설정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -38,5 +38,28 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+// 로우터 가드
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  // 토근이 존재할때 회원가입화면으로 가려고하면 메인으로 이동
+  // if (to.name === 'SignupView') {
+  //   if(token) {
+  //     next({ name:'MainView' })
+  //   }
+  // }
+  // 글쓰기페이지에서 토큰이 존재하지 않으면 로그인화면으로 이동
+  if (to.name === 'BoardCreateView') {
+    if(!token) {
+      next({ name:'MainView' })
+    }
+  }
+  // 글수정페이지에서 토큰이 존재하지 않으면 로그인화면으로 이동
+  else if (to.name === 'BoardEditView') {
+    if(!token) {
+      next({ name:'MainView' })
+    }
+  }
 
+  next()
+})
 export default router
